395f3f5805f45d7516ef9d1d313d5e662d6d9e36
Cameron <cameron_b@hotmail.co.uk>
2025-03-10 14:50:50 +0000

    Update vite config base for. Added npm install step to ci

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
+import { HashRouter as Router, Navigate, Route, Routes } from 'react-router';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 import Games from './pages/Games';
@@ -8,7 +8,7 @@ import Game from './pages/Game.tsx';
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
-		<BrowserRouter>
+		<Router>
 			<Routes>
 				<Route 
 					path="/"
@@ -24,6 +24,6 @@ createRoot(document.getElementById('root')!).render(
 					<Route path=":id" element={<Game />} />
 				</Route>
 			</Routes>
-		</BrowserRouter>
+		</Router>
 	</StrictMode>,
 )
